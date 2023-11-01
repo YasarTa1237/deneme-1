e658c14bac1aae78d65559f12ff054d082d2d023
YasarTa1237 <118163429+YasarTa1237@users.noreply.github.com>
2023-11-01 22:13:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import discord
+from discord.ext import commands
 
-word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
-meme_dict = {
-            "CRINGE": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
-            "EZ": "Çok Kolay Olan Bir Şeye Denir"
-            }
-if word in meme_dict.keys():
-    print("kelime var")
-else:
-    print("kelime sözlükte yok")
-    
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba {bot.user}! Ben bir botum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def aga_küfür_etme(ctx):
+    await ctx.send("::middle_finger:")
+
+bot.run("MTE2NjgwODI0MzI1MTcyODQ3Ng.GNIURB.q1crbk-xfrGKYO1-53OV-8SmKik_ajkDa4B9zk")
+
